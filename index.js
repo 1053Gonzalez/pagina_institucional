0382bf4a668fccb54bc36e14543d6114a7bb94b9
1053Gonzalez <jose.gonzalez.6000@miremington.edu.co>
2023-02-19 00:43:19 -0500

    Se agregan estilos del footer, se agrega imagen de fondo y se elimina el contenido de indes.js

--- index.js
+++
@@ -1,9 +0,0 @@
-const linea = document.querySelector('#linea');
-const menu = document.querySelector('nav');
-
-menu.addEventListener('click', e => {
-    if(e.target.nodeName == 'A'){
-        linea.style.left = e.target.offsetLeft + 'px';
-        linea.style.width = e.target.offsetWidth + 'px';
-    }
-})

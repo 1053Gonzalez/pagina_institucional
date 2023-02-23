dbeb75fe74705c626c1d3b7aa5151c6d0e9dd1ed
Edfer6281 <edison.pineda.6281@miremington.edu.co>
2023-02-23 17:47:39 -0500

    actualizacion de index.css

--- sources/js/pqrs.js
+++
@@ -1,8 +0,0 @@
-var getData = function () {
-    var nombre = document.getElementById("Nombre").value;
-    var correo = document.getElementById("Correo").value;
-    var telefono = document.getElementById("Telefono").value;
-    var mensaje = document.getElementById("Mensaje").value;
-
-        console.log(nombre+" "+correo+" "+telefono+" "+mensaje)
-    }

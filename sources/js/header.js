b5f1e7f5d63ed50c86b00db4516d0724fc93e9a9
1053Gonzalez <jose.gonzalez.6000@miremington.edu.co>
2023-02-22 23:28:17 -0500

    se agrega el archivo header.js con el contenido de la cabecera, se instancia como script en todas las paginas y subpaginas, se agrega el logo inst a la calculadora imc

---
+++ sources/js/header.js
@@ -0,0 +1,29 @@
+
+var header = document.createElement("header");
+header.className = "header-general";
+header.innerHTML = 
+'<nav class="header">'+
+    '<img src="/sources/img/logo.png" id="logo" />'+
+    '<div id="linea"></div>'+
+    '<ul class="menu">'+
+      '<li><a href="/index.html">Inicio</a></li>'+
+      '<li>'+
+        '<a href="">Matrículas</a>'+
+        '<ul class="submenu">'+
+          '<li><a href="/sources/forms/formPrescolar.html">Preescolar</a></li>'+
+          '<li><a href="/sources/forms/formPrimaria.html">Primaria</a></li>'+
+          '<li><a href="/sources/forms/formSecundaria.html">Secundaria</a></li>'+
+        '</ul>'+
+      '</li>'+
+      '<li><a href="/notas.html">Notas</a></li>'+
+      '<li><a href="/docentes.html">Docentes</a></li>'+
+      '<li><a href="/myv.html">Misión y Visión</a></li>'+
+      '<li><a href="/pqrs.html">PQRS</a></li>'+
+      '<li>'+
+        '<a href="">functions</a>'+
+        '<ul class="submenu">'+
+          '<li><a href="sources/function/imc.html">IMC</a></li>'+
+        '</ul>'+
+      '</li>'+
+  '</nav>'
+  document.body.appendChild(header);

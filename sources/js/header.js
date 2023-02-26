c3994d83aa16d793a97c8918cc02088a6d73cb0a
EstebanCS2203 <esteban.causado.8914@miremington.edu.co>
2023-02-26 11:05:45 -0500

    Se soluciona bug con estilos del elemento a con linea decorativa inferior

--- sources/js/header.js
+++ sources/js/header.js
@@ -6,23 +6,23 @@ header.innerHTML =
   '<img src="/sources/img/logo.png" id="logo" />' +
   '<div id="linea"></div>' +
   '<ul class="menu">' +
-  '<li><a href="/index.html">Inicio</a></li>' +
+  '<li><a class="header-line" href="/index.html">Inicio</a></li>' +
   '<li>' +
-  '<a href="">Matrículas</a>' +
+  '<a class="header-line" href="">Matrículas</a>' +
   '<ul class="submenu">' +
-  '<li><a href="/sources/forms/formPrescolar.html">Preescolar</a></li>' +
-  '<li><a href="/sources/forms/formPrimaria.html">Primaria</a></li>' +
-  '<li><a href="/sources/forms/formSecundaria.html">Secundaria</a></li>' +
+  '<li><a class="header-line" href="/sources/forms/formPrescolar.html">Preescolar</a></li>' +
+  '<li><a class="header-line" href="/sources/forms/formPrimaria.html">Primaria</a></li>' +
+  '<li><a class="header-line" href="/sources/forms/formSecundaria.html">Secundaria</a></li>' +
   '</ul>' +
   '</li>' +
-  '<li><a href="/notas.html">Notas</a></li>' +
-  '<li><a href="/docentes.html">Docentes</a></li>' +
-  '<li><a href="/myv.html">Misión y Visión</a></li>' +
-  '<li><a href="/pqrs.html">PQRS</a></li>' +
+  '<li><a class="header-line" href="/notas.html">Notas</a></li>' +
+  '<li><a class="header-line" href="/docentes.html">Docentes</a></li>' +
+  '<li><a class="header-line" href="/myv.html">Misión y Visión</a></li>' +
+  '<li><a class="header-line" href="/pqrs.html">PQRS</a></li>' +
   '<li>' +
-  '<a href="">functions</a>' +
+  '<a class="header-line" id="header-line" href="">functions</a>' +
   '<ul class="submenu">' +
-  '<li><a href="sources/function/imc.html">IMC</a></li>' +
+  '<li><a class="header-line" href="sources/function/imc.html">IMC</a></li>' +
   '</ul>' +
   '</li>' +
  '<div id="reloj"></div>'+
